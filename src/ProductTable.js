f83361818edb169aaf55e1f3ad43ed32240dfce2
hdehal <harry@hdehal.com>
2020-02-04 21:59:15 +0000

    Rename app and clean project

--- src/ProductTable.js
+++ src/ProductTable.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 // dummy data
-const products = [
+const data = [
   { id: 1, name: "Item 1", price: 100 },
   { id: 2, name: "Item 2", price: 102 }
 ];
@@ -19,5 +18,17 @@ const columns = [{
   text: 'Product Price'
 }];
 
-export default () =>
-  <BootstrapTable keyField='id' data={ products } columns={ columns } />
+const ProductTable = () => {
+  return (
+    <BootstrapTable
+      keyField="id"
+      data={data}
+      columns={columns}
+      striped
+      hover
+      condensed
+    />
+  );
+};
+
+export default ProductTable;
