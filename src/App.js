1926f1fc40b80ac5c755e9c68a28d324d8e95247
hdehal <harry@hdehal.com>
2020-02-07 02:43:58 +0000

    Setup basic app routing

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Create from './components/Create';
+import Edit from './components/Edit';
+import Index from './components/Index';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand"><span role="img" aria-label="US flag">🇺🇸</span> Made in USA List</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/index'} className="nav-link">Index</Link>
+                </li>
+              </ul>
+            </div>
+          </nav> <br/>
+          <h2>Welcome</h2> <br/>
+          <Switch>
+              <Route exact path='/create' component={ Create } />
+              <Route path='/edit/:id' component={ Edit } />
+              <Route path='/index' component={ Index } />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import ProductTable from './ProductTable';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<ProductTable />, document.getElementById('root'));
+import App from './App';
+
+ReactDOM.render(
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>, document.getElementById('root'));
