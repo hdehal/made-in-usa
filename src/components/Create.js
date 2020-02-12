7498e2f31cd4b0fdd06167623e1b5eacf0d165a6
hdehal <harry@hdehal.com>
2020-02-12 01:15:44 +0000

    Connect forms to submit data to db

--- src/components/Create.js
+++ src/components/Create.js
@@ -60,59 +60,7 @@ export default class Create extends Component {
 
     render() {
         return (
-            <div>
-                <h3>Add New Company</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Company Name:  </label>
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            value={this.state.company}
-                            onChange={this.onChangeCompanyName}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>URL: </label>
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            value={this.state.url}
-                            onChange={this.onChangeUrl}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Location: </label>
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            value={this.state.loc}
-                            onChange={this.onChangeLoc}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Gender: </label>
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            value={this.state.gender}
-                            onChange={this.onChangeGender}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Category/Tags: </label>
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            value={this.state.tags}
-                            onChange={this.onChangeTags}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Submit" className="btn btn-primary"/>
-                    </div>
-                </form>
-        </div>
+
         )
     }
 }
