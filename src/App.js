c8caae105ccb01d317bd83e1a9354f7fbdfa7364
hdehal <harry@hdehal.com>
2020-02-13 21:49:49 +0000

    Create sortFunc for JSON table column data

--- src/App.js
+++ src/App.js
@@ -22,6 +22,14 @@ const mongoClient = app.getServiceClient(
 // Define db and collection
 const item = mongoClient.db("vendor").collection("vendor-item");
 
+// Define sortCaret
+const sortFunc = (order, column) => {
+  if (!order) return (<span className="order"><span className="dropdown"><span className="caret"></span></span><span className="dropup"><span className="caret"></span></span></span>);
+  else if (order === 'asc') return (<span className="react-bootstrap-table-sort-order"><span className="caret"></span></span>);
+  else if (order === 'desc') return (<span className="react-bootstrap-table-sort-order dropup"><span className="caret"></span></span>);
+  return null;
+}
+
 // JSON table column data
 const columns = [
   {
@@ -33,24 +41,22 @@ const columns = [
     dataField: 'company',
     text: 'Company',
     sort: true,
-    sortCaret: (order, column) => {
-      if (!order) return (<span className="order"><span className="dropdown"><span className="caret"></span></span><span className="dropup"><span className="caret"></span></span></span>);
-      else if (order === 'asc') return (<span className="react-bootstrap-table-sort-order"><span className="caret"></span></span>);
-      else if (order === 'desc') return (<span className="react-bootstrap-table-sort-order dropup"><span className="caret"></span></span>);
-      return null;
-    }
+    sortCaret: sortFunc
 }, {
     dataField: 'url',
     text: 'URL',
-    sort: true
+    sort: true,
+    sortCaret: sortFunc
 }, {
     dataField: 'loc',
     text: 'Location',
-    sort: true
+    sort: true,
+    sortCaret: sortFunc
 }, {
     dataField: 'gender',
     text: 'Gender',
-    sort: true
+    sort: true,
+    sortCaret: sortFunc
 }, {
     dataField: 'tags',
     text: 'Tags',
