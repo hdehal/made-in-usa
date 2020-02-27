79500bb82a1e56186e4993a3389228f19fe0b515
hdehal <harry@hdehal.com>
2020-02-27 07:37:49 +0000

    Added API auth and moved API keys to ENV file

--- src/components/stitchAuth.js
+++ src/components/stitchAuth.js
@@ -1,4 +1,4 @@
-import { Stitch, RemoteMongoClient } from 'mongodb-stitch-browser-sdk';
+import { Stitch, RemoteMongoClient, UserApiKeyCredential } from 'mongodb-stitch-browser-sdk';
 
 // Define MongoDB Stitch App ID
 export const APP_ID = "miusa-gxhmx";
@@ -6,13 +6,25 @@ export const APP_ID = "miusa-gxhmx";
 // Initialize MongoDB Stitch
 export const app = Stitch.hasAppClient(APP_ID)
     ? Stitch.getAppClient(APP_ID)
-    : Stitch.initializeAppClient(APP_ID);
+    : Stitch.initializeDefaultAppClient(APP_ID);
+    // OR for anonymous login use:
+    // Stitch.initializeAppClient(APP_ID); 
 
 // Define client/factory
 export const mongoClient = app.getServiceClient(
     RemoteMongoClient.factory,
     "mongodb-atlas"
-  );  
+  );
 
 // Define db and collection
 export const item = mongoClient.db("vendor").collection("vendor-item");
+
+// API authentication
+// https://stitch.mongodb.com > (your app) > Users > Providers > API Key (On) && Users > Add
+const credential = new UserApiKeyCredential(process.env.REACT_APP_STITCH_API_KEY)
+
+Stitch.defaultAppClient.auth.loginWithCredential(credential)
+  .then(authedId => {
+     console.log(`Successfully connected to the database!`);
+  })
+  .catch(err => console.error(`Login failed with error: ${err}`));
