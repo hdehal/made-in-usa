eca5cfafeda5b765eb54053d9fec87142af6bd1a
hdehal <harry@hdehal.com>
2020-02-29 20:35:24 +0000

    Convert Alerts to controlled component

--- src/components/alerts.js
+++ src/components/alerts.js
@@ -1,21 +1,40 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Alert from 'react-bootstrap/Alert'
 
-function AlertDismissible() {
-    const [show, setShow] = useState(true);
-  
-    return (
+class Alerts extends Component {
+
+constructor(props){
+    super(props);
+
+    this.onClickAlert = this.onClickAlert.bind(this);
+
+    this.state = {
+        setShow: true
+    }
+  }
+
+// Alerts onChange
+onClickAlert(e) {
+    this.setState({
+        setShow: false
+});
+}
+
+  render() {
+
+    return(
         <div className="mainContainer">
-        <Alert show={show} variant="success" onClick={() => setShow(false)} dismissible>
-          <Alert.Heading>Test Heading</Alert.Heading>
-          <p>
+        <Alert show={this.state.setShow} variant="success" onClick={this.onClickAlert} dismissible>
+            <Alert.Heading>Test Heading</Alert.Heading>
+            <p>
             Test Message Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
             lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
             fermentum.
-          </p>
+            </p>
         </Alert>
-      </div>
+        </div>
     );
-  }
-  
-export default AlertDismissible;
+    }
+    }
+
+export default Alerts;
