3aaaa7b072a06888fd99cb77a8508217c622c20e
hdehal <harry@hdehal.com>
2020-03-02 06:35:39 +0000

    Update basename, homepage, routes, links, cname

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,7 @@ const About = () => {
             <Alert variant="primary">
             <Alert.Heading>About the List</Alert.Heading>
             <p>
-                Over ten years ago, I took a challenge to slowly convert my wardrobe to entirely "Made in USA" clothing. The goals were numerous — from having fewer items of higher-quality clothing that would last longer, to voting with my wallet and making a political statement for fair wages, supply chain transparency, and against adverse working conditions abroad. Not all companies listed have their entire product portfolio American-made —  the only requirement is that some product(s) should be manufactured in the USA — regardless of domestic or foreign materials. Please feel free to <NavLink to="/add">contribute</NavLink>!  
+                Over ten years ago, I took a challenge to slowly convert my wardrobe to entirely "Made in USA" clothing. The goals were numerous — from having fewer items of higher-quality clothing that would last longer, to voting with my wallet and making a political statement for fair wages, supply chain transparency, and against adverse working conditions abroad. Not all companies listed have their entire product portfolio American-made —  the only requirement is that some product(s) should be manufactured in the USA — regardless of domestic or foreign materials. Please feel free to <NavLink to={`${process.env.PUBLIC_URL}/add`}>contribute</NavLink>!  
             </p>
             <hr />
             <p className="mb-0">
