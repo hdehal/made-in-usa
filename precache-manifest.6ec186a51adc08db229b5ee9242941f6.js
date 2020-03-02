e3388eb50eb5263a93f4824a8d62a28e1d1e8e6e
hdehal <harry@hdehal.com>
2020-03-02 08:11:57 +0000

    Updates

---
+++ precache-manifest.6ec186a51adc08db229b5ee9242941f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38fc92fbec2db10be65ec006b6d9e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a308032893bdff15932",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b3bd892231a3cc14f5d4",
+    "url": "/static/css/main.9ef9ca9d.chunk.css"
+  },
+  {
+    "revision": "0a308032893bdff15932",
+    "url": "/static/js/2.b8914b77.chunk.js"
+  },
+  {
+    "revision": "ce476f46cb051f8ffa2b2ac1be25d103",
+    "url": "/static/js/2.b8914b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3bd892231a3cc14f5d4",
+    "url": "/static/js/main.edf4ba7a.chunk.js"
+  },
+  {
+    "revision": "76476c2aef081601d845",
+    "url": "/static/js/runtime-main.c5aa4d74.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/made-in-usa/precache-manifest.a706f0461f54426ba2a98c7269953ae1.js"
+  "/precache-manifest.6ec186a51adc08db229b5ee9242941f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/made-in-usa/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
