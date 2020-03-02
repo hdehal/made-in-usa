948613f71ea53428704143011bb14f9a3475c1ef
hdehal <harry@hdehal.com>
2020-03-02 07:20:50 +0000

    Updates

---
+++ precache-manifest.45cfd7ecf2359e93dff4678a73953fe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec5cab4a43d11e025b34d724d950e5f",
+    "url": "/made-in-usa/index.html"
+  },
+  {
+    "revision": "0a308032893bdff15932",
+    "url": "/made-in-usa/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "79e86bb4bf5c02e81a90",
+    "url": "/made-in-usa/static/css/main.9ef9ca9d.chunk.css"
+  },
+  {
+    "revision": "0a308032893bdff15932",
+    "url": "/made-in-usa/static/js/2.b8914b77.chunk.js"
+  },
+  {
+    "revision": "ce476f46cb051f8ffa2b2ac1be25d103",
+    "url": "/made-in-usa/static/js/2.b8914b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e86bb4bf5c02e81a90",
+    "url": "/made-in-usa/static/js/main.c07fee6b.chunk.js"
+  },
+  {
+    "revision": "f412d381685923c69ab6",
+    "url": "/made-in-usa/static/js/runtime-main.6ec9bbe0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/made-in-usa/precache-manifest.45cfd7ecf2359e93dff4678a73953fe8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/made-in-usa/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
