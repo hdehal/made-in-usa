846eaffbd8540c130b9858b693632fc932bc1107
hdehal <harry@hdehal.com>
2020-03-05 19:43:59 +0000

    Switching from static to state dataMaps

--- src/components/maps.js
+++ src/components/maps.js
@@ -49,25 +49,20 @@ class App extends Component {
       
           componentDidMount(){
             this.getData();
+            console.log(this.state.dataMaps)
+          }
+          
+          componentDidUpdate(){
+            console.log(this.state.dataMaps)
           }
 
   render() {
-    var centerLat = (data.minLat + data.maxLat) / 2;
-    var distanceLat = data.maxLat - data.minLat;
-    var bufferLat = distanceLat * 0.05;
-    var centerLong = (data.minLong + data.maxLong) / 2;
-    var distanceLong = data.maxLong - data.minLong;
-    var bufferLong = distanceLong * 0.05;
+
     return (
       <div>
         <Map
           style={{ height: "480px", width: "100%", opacity: "0.9" }}
           zoom={1}
-          center={[centerLat, centerLong]}
-          bounds={[
-            [data.minLat - bufferLat, data.minLong - bufferLong],
-            [data.maxLat + bufferLat, data.maxLong + bufferLong]
-          ]}
           attributionControl={false}>
           <TileLayer url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png" 
           attribution="Map by <a href='http://stamen.com' target='_blank'>Stamen Design</a> | Data &copy; <a href='https://www.openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors"
@@ -75,11 +70,11 @@ class App extends Component {
 
         <AttributionControl position="bottomright" prefix={false} />
 
-          {data.city.map((city, k) => {
+          {this.state.dataMaps.map((dataMaps, k) => {
             return (
               <CircleMarker
                 key={k}
-                center={[city["coordinates"][1], city["coordinates"][0]]}
+                center={[dataMaps["coordinates"][1], dataMaps["coordinates"][0]]}
                 /* radius={20 * Math.log(city["population"] / 10000000)} */
                 fillOpacity={0.5}
                 stroke={false}>
