c2210f306b6651dfa5b2b41baf7efe25d074e247
hdehal <harry@hdehal.com>
2020-03-12 17:37:14 +0000

    Add conditional styling logic

--- src/components/scrollTop.js
+++ src/components/scrollTop.js
@@ -18,10 +18,12 @@ window.addEventListener("scroll", scrollFunc);
 class ScrollTop extends React.Component {
     render() {
         return (
-            <div className="scrolltop" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+            <div className="scrollTop"
+                onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+                style={{ visibility: showScrollTop === false ? 'visible' : 'hidden' }}
+            >
                 Top
             </div>
-
         );
     }
 }
