beb2a3d14a250e381cb37444ee1e379f328b4538
hdehal <harry@hdehal.com>
2020-03-12 17:33:19 +0000

    Add scrollFunc event listener

--- src/components/scrollTop.js
+++ src/components/scrollTop.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+let showScrollTop = false;
+
+var scrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 800) {
+        showScrollTop = true;
+        console.log(showScrollTop)
+    } else {
+        showScrollTop = false;
+        console.log(showScrollTop)
+    }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
 class ScrollTop extends React.Component {
     render() {
         return (
